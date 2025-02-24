8b2d094d9457f021a029af469cd72d7bb83b629d
yfc0307 <155843600+yfc0307@users.noreply.github.com>
2025-02-24 20:51:04 +0800

    modity mailsender
    fixed a bug that when user does not have borrow record, mailsender fatal error

--- mailsender.php
+++ mailsender.php
@@ -16,27 +16,51 @@ while ($row = mysqli_fetch_array($result)) {
        
 }
 
-foreach ($borrowid as $id) {
+if ($borrowid) {
+    foreach ($borrowid as $id) {
 
-    $x = $id - 1;
-
-    $d = strtotime($d);
-    $d1 = strtotime($rdate[$x]);
-    $d2 = ceil(($d1 - $d)/60/60/24);
-
-    if ($d2 < 0) {
-        $d2 = abs($d2);
-        $mailmsg = "Your book no. $bookid[$x] returning date has expired for $d2 days. Please return as soon as possible.";
-    } elseif ($d2 > 0) {
-        $mailmsg = "Your book no. $bookid[$x] returning date will expire in $d2 days. Please return before $rdate[$x]";
-    } else {
-        $mailmsg = "Your book no. $bookid[$x] returning date will expire today.";
+        $x = $id - 1;
+    
+        $d = strtotime($d);
+        $d1 = strtotime($rdate[$x]);
+        $d2 = ceil(($d1 - $d)/60/60/24);
+    
+        if ($d2 < 0) {
+            $d2 = abs($d2);
+            $mailmsg = "Your book no. $bookid[$x] returning date has expired for $d2 days. Please return as soon as possible.";
+        } elseif ($d2 > 0) {
+            $mailmsg = "Your book no. $bookid[$x] returning date will expire in $d2 days. Please return before $rdate[$x]";
+        } else {
+            $mailmsg = "Your book no. $bookid[$x] returning date will expire today.";
+        }
+    
+        $sql_mailsender = "insert into mail (senderid, receiverid, title, content) values ('0', '$userid[0]', 'Book Return Reminder', '$mailmsg')";
     }
-
-    $sql_mailsender = "insert into mail (senderid, receiverid, title, content) values ('0', '$userid[0]', 'Book Return Reminder', '$mailmsg')";
+} else {
+    $mailmsg = "";
 }
 
-$query = mysqli_query($conn,$sql_mailsender);
+////////// The following codes are yet tested (24/2/2025) //////////
+
+// Prevent sending multiple same mail when user login multiple times within a day
+
+if ($mailmsg != ""){
+    $sql_checkmail = "select count(*) from mail m, users u where m.receiverid = u.userid and m.content = '$mailmsg'"; // Check if exist same content mail
+    $check_result = mysqli_query($conn,$sql_checkmail);
+
+    while ($fetched_check_result = $check_result->fetch_assoc()) {
+        foreach ($fetched_check_result as $a) {
+            echo $a;
+        }
+    }
+
+    if ($a == 0) {
+        $query = mysqli_query($conn,$sql_mailsender);
+    }
+}
+
 header('Location: homepage.php');
 exit;
+
+////////// The above codes are yet tested (24/2/2025) //////////
 ?>
